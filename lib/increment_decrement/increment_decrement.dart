b277cb4362610f87ae4f2ef65ee3b2aa24037b70
Janshair Khan <janshairkhan@Janshairs-MacBook-Pro.local>
2020-06-17 14:17:51 +0100

    Initial Commit

---
+++ lib/increment_decrement/increment_decrement.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:blocpractise/increment_decrement/increment_decrement_bloc.dart';
+
+import 'package:blocpractise/increment_decrement/counter_event.dart';
+
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final IncrementDecrementBloc counterBloc = BlocProvider.of<IncrementDecrementBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<IncrementDecrementBloc, int>(
+        builder: (context, count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                counterBloc.add(CounterEvent.increment);
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () {
+                counterBloc.add(CounterEvent.decrement);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:blocpractise/timer/ticker.dart';
+import 'package:blocpractise/timer/timer_bloc.dart';
+import 'package:blocpractise/timer/ui/timer_page.dart';
+import 'package:blocpractise/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:blocpractise/increment/increment.dart';
+import 'package:blocpractise/increment_decrement/increment_decrement_bloc.dart';
+import 'package:blocpractise/increment_decrement/increment_decrement.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+      ),
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
+
+//home: IncrementPage()
+
+//home: BlocProvider<CounterBloc>(
+//create: (context) => CounterBloc(),
+//child: CounterPage()),
+
+//BlocProvider(
+//create: (context) => TimerBloc(ticker: Ticker()),
+//child: Timer(),
+//),
